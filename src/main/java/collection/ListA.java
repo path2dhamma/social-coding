1ff5328ca883acaafeda22af7825df12dd3363eb
path2dhamma <57743915+path2dhamma@users.noreply.github.com>
2019-11-14 16:28:05 +0900

    [path2dhamma] 코드가독성 개선작업 #6 #5
    #6   Masic number 제거
    #5  If문 부정형을 긍정형으로 변경

--- src/main/java/collection/ListA.java
+++ src/main/java/collection/ListA.java
@@ -1,16 +1,19 @@
 package collection;
 
 public class ListA {
+	private static final int STORE_SIZE_INCREMENT = 10;
+	
 	public Object[] elements = new Object[10];
 	public boolean readOnly;
 	public int size = 0;
 
 	public void add(Object element) {
-		if (!readOnly) {
+		if (readOnly) {
+		} else {				
 			int newSize = size + 1;
 			
 			if ( newSize > elements.length) {
-				Object[] newElements = new Object[elements.length + 10];
+				Object[] newElements = new Object[elements.length + STORE_SIZE_INCREMENT];
 				for (int i = 0; i < size; i++) {
 					newElements[i] = elements[i];
 				}
